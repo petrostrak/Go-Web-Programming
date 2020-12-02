f1f28a24899b5f9d0ab2c509cb95416ddf5fee5d
petros <pit.trak@gmail.com>
2020-12-02 21:56:00 +0200

    func generateHTML

--- util.go
+++ util.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func session(w http.ResponseWriter, r *http.Request) (s data.Session, err error) {
-	cookie, err := r.Cookie("_cookie")
+func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
+	cookie, err := request.Cookie("_cookie")
 	if err == nil {
-		s = data.Session{Uuid: cookie.Value}
-		if ok, _ := s.Check(); !ok {
+		sess = data.Session{Uuid: cookie.Value}
+		if ok, _ := sess.Check(); !ok {
 			err = errors.New("Invalid session")
 		}
 	}
 	return
+}
+
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
 }
