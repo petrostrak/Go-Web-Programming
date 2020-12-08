84ebe2ac1a49391af5a0c20425e1bc50593c7570
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-08 09:36:33 +0200

    Update main.go

--- 03. Handling Requests/Using HttpRouter/main.go
+++ 03. Handling Requests/Using HttpRouter/main.go
@@ -9,15 +9,15 @@ import (
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintf(w, "Hello %s!", p.ByName("name"))
-} 
+}
 
 func main() {
 	mux := httprouter.New()
-	mux.Get("/hello/:name", hello)
+	mux.GET("/hello/:name", hello)
 
 	server := http.Server{
-		Addr: "127.0.0.0:8080",
-		Handler: mux
+		Addr:    "127.0.0.0:8080",
+		Handler: mux,
 	}
 	server.ListenAndServe()
 }
