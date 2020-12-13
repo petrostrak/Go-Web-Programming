2a0404210afbab10759b9af10b32c7316a9e7b6c
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-13 16:34:49 +0200

    Retrieving uploaded files using FormFile

--- 04. Processing requests/04. Receiving uploaded files using the MultipartForm field/main.go
+++ 04. Processing requests/04. Receiving uploaded files using the MultipartForm field/main.go
@@ -6,10 +6,21 @@ import (
 	"net/http"
 )
 
+// func process(w http.ResponseWriter, r *http.Request) {
+// 	r.ParseMultipartForm(1024)
+// 	fileHeader := r.MultipartForm.File["uploaded"][0]
+// 	file, err := fileHeader.Open()
+// 	if err == nil {
+// 		data, err := ioutil.ReadAll(file)
+// 		if err == nil {
+// 			fmt.Fprintln(w, string(data))
+// 		}
+// 	}
+// }
+
+// Retrieving uploaded files using FormFile
 func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
+	file, _, err := r.FormFile("uploaded")
 	if err == nil {
 		data, err := ioutil.ReadAll(file)
 		if err == nil {
