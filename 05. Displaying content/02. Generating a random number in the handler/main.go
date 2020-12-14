d0e1ba887a97b9bf1f1d156bc90add68364455be
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-14 18:56:14 +0200

    Generating a random number in the handler

--- 05. Displaying content/02. Generating a random number in the handler/main.go
+++ 05. Displaying content/02. Generating a random number in the handler/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("tmpl.html")
-	t.Execute(w, "Hello World from Go template engine!")
-}
-
-func processTwo(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("tmpl.html")
-	t.Execute(w, "Hi from the second template")
+	rand.Seed(time.Now().Unix())
+	t.Execute(w, rand.Intn(10) > 5)
 }
 
 func main() {
@@ -20,6 +18,5 @@ func main() {
 		Addr: "127.0.0.1:8080",
 	}
 	http.HandleFunc("/process", process)
-	http.HandleFunc("/processTwo", processTwo)
 	server.ListenAndServe()
 }
