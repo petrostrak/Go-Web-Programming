bce543f1ade4d9ebba5e01f22495c90ad3af0836
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-14 16:27:02 +0200

    Update main.go

--- 04. Processing requests/12. Implementing flash messaging using Go cookies/main.go
+++ 04. Processing requests/12. Implementing flash messaging using Go cookies/main.go
@@ -11,7 +11,7 @@ func setMessage(w http.ResponseWriter, r *http.Request) {
 	msg := []byte("Hello World!")
 	c := http.Cookie{
 		Name: "flash",
-		Value: base64.URLEncoding.EncodeToString(msg)
+		Value: base64.URLEncoding.EncodeToString(msg),
 	}
 	http.SetCookie(w, &c)
 }
