f2ba3fbaffbddad4f77203e63e09b3f64531992b
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-14 13:23:31 +0200

    Using the Cookie and Cookies methods

---
+++ 04. Processing requests/11. Using the Cookie and Cookies methods/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	cs := r.Cookies()
+	fmt.Fprintln(w, c1)
+	fmt.Fprintln(w, cs)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/set_cookie", setCookie)
+	http.HandleFunc("/get_cookie", getCookie)
+	server.ListenAndServe()
+}
+
+/*
+Go provides a Cookie method on Request that
+allows you to retrieve a named cookie. If the cookie doesn’t exist, it’ll throw an error.
+This is a single value, though, so if you want to get multiple cookies you can use the
+Cookies method on Request. That way, you retrieve all cookies into a Go slice; in fact,
+it’s the same as getting it through the Header yourself.
+http://127.0.0.1:8080/get_cookie
+*/
