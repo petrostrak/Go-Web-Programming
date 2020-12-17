7b3fa79101c6812a3fb0e4b4c7c5af2284183e00
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-17 18:06:41 +0200

    Go and SQL - store.go

---
+++ 06. Storing Data/05. Go and SQL/store.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+var Db *sql.DB
+
+// Connects to DB
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=root sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Posts(limit int) (posts []Post, err error) {
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+// Gets single post
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+// Creates a new post
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+// Updates a post
+func (post *Post) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+// Deletes a post
+func (post *Post) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
+
+func main() {
+	post := Post{Content: "Hello World!", Author: "Petros Trak"}
+
+	fmt.Println(post)
+	post.Create()
+	fmt.Println(post)
+
+	readPost, _ := GetPost(post.Id)
+	fmt.Println(readPost)
+
+	readPost.Content = "Bonjour Monde!"
+	readPost.Author = "Giannis"
+	readPost.Update()
+
+	// posts, _ := Posts()
+	// fmt.Println(posts)
+
+	readPost.Delete()
+}
