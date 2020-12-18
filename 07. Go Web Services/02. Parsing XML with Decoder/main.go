7ef1b395ff2635aca8f55ced8881efccea39ba7f
pit_trak <45797153+petrostrak@users.noreply.github.com>
2020-12-18 22:10:32 +0200

    Parsing XML with Decoder

---
+++ 07. Go Web Services/02. Parsing XML with Decoder/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Post struct {
+	XMLName  xml.Name  `xml:"post"`
+	Id       string    `xml:"id,attr"`
+	Content  string    `xml:"content"`
+	Author   Author    `xml:"author"`
+	Xml      string    `xml:",innerxml"`
+	Comments []Comment `xml:"comments>comment"`
+}
+
+type Author struct {
+	Id   string `xml:"id,attr"`
+	Name string `xml:"chardata"`
+}
+
+type Comment struct {
+	Id      string `xml:"id,attr"`
+	Content string `xml:"content"`
+	Author  Author `xml:"author"`
+}
+
+func main() {
+	xmlFile, err := os.Open("post.xml")
+	if err != nil {
+		fmt.Println("Error opening XML file:", err)
+		return
+	}
+	defer xmlFile.Close()
+
+	// Creates decoder from XML data
+	decoder := xml.NewDecoder(xml)
+	// Iterates through XML data in decoder
+	for {
+		// Gets token from decoder at each iteration
+		r, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error decoding XML into tokens:", err)
+			return
+		}
+
+		// Checks type of tokens
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "comment" {
+				// Decodes XML data into struct
+				decoder.DecodeElement(&comment, &se)
+			}
+		}
+	}
+}
