9115c9d9ebdfcaab6d3ca030126e7e01e7d81d38
petrostrak <pit.trak@gmail.com>
2020-12-18 12:07:36 +0200

    Added Comment struct and create single comment

---
+++ 06. Storing Data/06. One-to-many and many-to-one with Go/store.go
@@ -0,0 +1,128 @@
+package main
+
+// go get "github.com/lib/pq"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	Id       int
+	Content  string
+	Author   string
+	Comments []Comment
+}
+
+type Comment struct {
+	Id      int
+	Content string
+	Author  string
+	Post    *Post
+}
+
+var Db *sql.DB
+
+// Connects to DB
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=gwp dbname=gwp password=root sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Creates a new Comment
+func (comment *Comment) Create() (err error) {
+	if comment.Post == nil {
+		err = errors.New("Post not found")
+		return
+	}
+	err = Db.QueryRow("insert into comments (content, author, post_id) values ($1, $2, $3) returning id", comment.Content, comment.Author, comment.Post.Id).Scan(&comment.Id)
+	return
+}
+
+// Gets all posts
+func Posts(limit int) (posts []Post, err error) {
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+// Gets single post
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	post.Comments = []Comment{}
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	rows, err := Db.Query("select id, content, author from comments")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		comment := Comment{Post: &post}
+		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	rows.Close()
+	return
+}
+
+// Creates a new post
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+// Updates a post
+func (post *Post) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+// Deletes a post
+func (post *Post) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
+
+func main() {
+	post := Post{Content: "Hello World!", Author: "Petros Trak"}
+
+	fmt.Println(post)
+	post.Create()
+	fmt.Println(post)
+
+	readPost, _ := GetPost(post.Id)
+	fmt.Println(readPost)
+
+	readPost.Content = "Bonjour Monde!"
+	readPost.Author = "Giannis"
+	readPost.Update()
+
+	// posts, _ := Posts()
+	// fmt.Println(posts)
+
+	readPost.Delete()
+}
