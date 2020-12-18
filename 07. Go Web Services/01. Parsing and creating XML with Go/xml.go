6b231ce28f08bacb11f55c90163d34cd376de88d
petrostrak <pit.trak@gmail.com>
2020-12-18 16:25:11 +0200

    some rules for the XML struct tags

--- 07. Go Web Services/01. Parsing and creating XML with Go/xml.go
+++ 07. Go Web Services/01. Parsing and creating XML with Go/xml.go
@@ -8,6 +8,12 @@ import (
 )
 
 // Defines structs to represent the data
+// `xml:"post"` called struct tag and Go determines the mapping
+// between the struct and the XML elements using them.
+// Struct tags are strings after each field that are a key-value pair.
+// The key is a string and must not have a space, a quote ("), or a colon (:).
+// The value must be a string between double quotes ("").
+// For XML , the key must always be xml .
 type Post struct {
 	XMLName xml.Name `xml:"post"`
 	Id      string   `xml:"id,attr"`
@@ -39,3 +45,28 @@ func main() {
 	xml.Unmarshal(xmlData, &post)
 	fmt.Println(post)
 }
+
+/*
+Here are some rules for the XML struct tags:
+To store the name of the XML element itself (normally the name of the struct is
+the name of the element), add a field named XMLName with the type xml.Name .
+The name of the element will be stored in that field.
+
+To store the attribute of an XML element, define a field with the same name as
+that attribute and use the struct tag `xml:"<name>,attr"` , where <name> is the
+name of the XML attribute.
+
+To store the character data value of the XML element, define a field with the
+same name as the XML element tag, and use the struct tag `xml:",chardata"`.
+
+To get the raw XML from the XML element, define a field (using any name) and
+use the struct tag `xml:",innerxml"` .
+
+If there are no mode flags (like ,attr or ,chardata or ,innerxml ) the struct
+field will be matched with an XML element that has the same name as the
+struct’s name.
+
+To get to an XML element directly without specifying the tree structure to get to
+that element, use the struct tag `xml:"a>b>c"` , where a and b are the interme-
+diate elements and c is the node that you want to get to.
+*/
